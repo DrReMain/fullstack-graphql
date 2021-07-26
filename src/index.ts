b7ad4f8dd44b70edc1c0673dae6a3fe0504f8f41
DrReMain <51071534@qq.com>
2021-07-27 04:04:36 +0800

    feat: graphql express

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,34 @@
  * @copyright DrReMain
  * @author DrReMain
  */
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  orm.em.persistAndFlush(post);
-  console.log("--------------------sql 2--------------------");
-  await orm.em.nativeInsert(Post, { title: "my first post 2" });
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
